3adf5c557d35b9c94b54291a0b8a8e170cc27efd
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-03-10 16:35:02 -0500

    Formik added

--- src/components/Auth/RegisterForm/RegisterForm.tsx
+++ src/components/Auth/RegisterForm/RegisterForm.tsx
@@ -1,6 +1,9 @@
 import {Dispatch, SetStateAction} from 'react';
 
 import {Button, Form} from 'semantic-ui-react';
+import {FormikValues, useFormik} from 'formik';
+
+import {IRegisterUserInput} from '../../../interfaces/interfaces';
 
 import './RegisterForm.scss';
 
@@ -8,45 +11,75 @@ interface IProps {
   setShowLogin: Dispatch<SetStateAction<Boolean>>;
 }
 
+const initialValues: IRegisterUserInput = {
+  name: '',
+  username: '',
+  email: '',
+  password: '',
+  repeatPassword: '',
+};
+
 export const RegisterForm = (props: IProps) => {
   const {setShowLogin} = props;
 
-  const onSubmit = () => {
-    console.log('Formulario enviado');
-  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema: null,
+    onSubmit: (formValue: FormikValues) => {
+      console.log(formValue);
+    },
+  });
 
   return (
     <>
       <h2 className="register-form-title">
         Regístrate para ver fotos y vídeos de tus amigos.
       </h2>
-      <Form className="register-form" onSubmit={onSubmit}>
+      <Form className="register-form" onSubmit={formik.handleSubmit}>
         <Form.Input
           type="text"
           placeholder="Nombre y apellidos"
-          name="name"/>
+          name="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
 
         <Form.Input
           type="text"
           placeholder="Nombre de usuario"
-          name="username"/>
+          name="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+        />
 
         <Form.Input
           type="text"
           placeholder="Correo electronico"
-          name="email"/>
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
 
         <Form.Input
           type="password"
           placeholder="Contraseña"
-          name="password"/>
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
 
         <Form.Input
-          type="text"
+          type="password"
           placeholder="Repetir contraseña"
-          name="repeatPassword"/>
+          name="repeatPassword"
+          value={formik.values.repeatPassword}
+          onChange={formik.handleChange}
+        />
 
         <Button type="submit" className="btn-submit">Registrarse</Button>
+        <Button type="button" onClick={formik.handleReset}>
+          Reiniciar formulario
+        </Button>
       </Form>
     </>
   );
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+export interface IUser {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  avatar?: string;
+  web?: string;
+  description?: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IRegisterUserInput extends Pick<IUser, 'name' | 'username' | 'email' | 'password'> {
+  repeatPassword: string;
+}
