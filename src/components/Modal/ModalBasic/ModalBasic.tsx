c343bfe6811728aa254f25fb57e6eb698b9c1d16
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-03-18 12:26:48 -0500

    Modal added

---
+++ src/components/Modal/ModalBasic/ModalBasic.tsx
@@ -0,0 +1,35 @@
+import {Modal} from 'semantic-ui-react';
+
+import './ModalBasic.scss';
+
+interface IProps {
+  showModal: boolean;
+  setShowModal: (showModal: boolean) => void ;
+  title?: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+export const ModalBasic = (props: IProps) => {
+  const {showModal, setShowModal, title, children} = props;
+
+  const onClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <Modal
+      size="mini"
+      open={showModal}
+      onClose={onClose}
+      className="modal-basic"
+    >
+      {
+        (title) && (
+          <Modal.Header>{title}</Modal.Header>
+        )
+      }
+
+      {children}
+    </Modal>
+  );
+};
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,21 @@
+import {useState} from 'react';
+
 import {useQuery} from '@apollo/client';
 import {Grid, Image} from 'semantic-ui-react';
-
 import './Profile.scss';
-import ImageNoFound from '../../assets/png/avatar.png';
 
+import ImageNoFound from '../../assets/png/avatar.png';
 import {GET_USER} from '../../gql/user';
 import {UserNotFound} from '../UserNotFound/UserNotFound';
+import {ModalBasic} from '../Modal/ModalBasic/ModalBasic';
 
 interface IProps {
   username: string;
 }
 
 export const Profile = ({username}: IProps) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   const {data, loading, error} = useQuery(GET_USER, {
     variables: {
       username,
@@ -32,6 +36,7 @@ export const Profile = ({username}: IProps) => {
           <Image
             src={ImageNoFound}
             avatar
+            onClick={() => setShowModal(true)}
           />
         </Grid.Column>
 
@@ -70,6 +75,14 @@ export const Profile = ({username}: IProps) => {
           </div>
         </Grid.Column>
       </Grid>
+
+      <ModalBasic
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title="Subir Avatar"
+      >
+        <p>Opciones...</p>
+      </ModalBasic>
     </>
   );
 };
