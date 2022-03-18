49a4636fea68b84b0c283f94ae734b2f7f7cb1e9
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-03-18 00:11:11 -0500

    User profile structure

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,10 @@
-import './Profile.scss';
 import {useQuery} from '@apollo/client';
+
+import './Profile.scss';
+import ImageNoFound from '../../assets/png/avatar.png';
+
 import {GET_USER} from '../../gql/user';
+import {Grid, Image} from 'semantic-ui-react';
 
 interface IProps {
   username: string;
@@ -15,10 +19,56 @@ export const Profile = ({username}: IProps) => {
 
   if (loading) return null;
   if (error) return <h1>Usuario no encontrado</h1>;
+  const {getUser} = data;
 
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <>
+      <Grid className="profile">
+        <Grid.Column
+          width={5}
+          className="profile__left"
+        >
+          <Image
+            src={ImageNoFound}
+            avatar
+          />
+        </Grid.Column>
+
+        <Grid.Column
+          width={11}
+          className="profile__right"
+        >
+          <div>
+            Header Profile
+          </div>
+
+          <div>
+            Followers
+          </div>
+
+          <div className="other">
+            <p className="name">{getUser.name}</p>
+            {
+              (getUser.web) && (
+                <a
+                  href={getUser.web}
+                  className="siteWeb"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {getUser.web}
+                </a>
+              )
+            }
+
+            {
+              (getUser.description) && (
+                <p className="description">{getUser.description}</p>
+              )
+            }
+          </div>
+        </Grid.Column>
+      </Grid>
+    </>
   );
 };
