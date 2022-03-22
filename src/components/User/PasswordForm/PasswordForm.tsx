c97547a0beffcbf83d96cbedcb90b144cb63d249
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-03-22 17:55:47 -0500

    Formik in the password change form

--- src/components/User/PasswordForm/PasswordForm.tsx
+++ src/components/User/PasswordForm/PasswordForm.tsx
@@ -1,22 +1,72 @@
 import {Button, Form} from 'semantic-ui-react';
+import {FormikValues, useFormik} from 'formik';
+import * as Yup from 'yup';
 import './PasswordForm.scss';
 
+interface IInitialValues {
+  currentPassword: string;
+  newPassword: string;
+  repeatNewPassword: string;
+}
+
+const initialValues: IInitialValues = {
+  currentPassword: '',
+  newPassword: '',
+  repeatNewPassword: '',
+};
+
+const validationSchema = Yup.object({
+  currentPassword: Yup.string()
+      .required('La contraseña es obligatorip'),
+  newPassword: Yup.string()
+      .required('La nueva contraseña es obligatorio')
+      .oneOf([Yup.ref('repeatNewPassword')], 'Las contraseñas no son iguales'),
+  repeatNewPassword: Yup.string()
+      .required('La nueva contraseña es obligatorio')
+      .oneOf([Yup.ref('newPassword')], 'Las contraseñas no son iguales'),
+});
+
 export const PasswordForm = () => {
+  const onSubmit = (values: FormikValues) => {
+    console.log('enviado');
+    console.log(values);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
   return (
-    <Form className="password-form">
+    <Form className="password-form" onSubmit={formik.handleSubmit}>
       <Form.Input
+        type="password"
         placeholder="Contraseña actual"
         name="currentPassword"
+        value={formik.values.currentPassword}
+        onChange={formik.handleChange}
+        error={formik.touched.currentPassword && formik.errors.currentPassword}
       />
 
       <Form.Input
+        type="password"
         placeholder="Nueva contraseña"
         name="newPassword"
+        value={formik.values.newPassword}
+        onChange={formik.handleChange}
+        error={formik.touched.newPassword && formik.errors.newPassword}
       />
 
       <Form.Input
+        type="password"
         placeholder="Repetir nueva contraseña"
         name="repeatNewPassword"
+        value={formik.values.repeatNewPassword}
+        onChange={formik.handleChange}
+        error={
+          formik.touched.repeatNewPassword && formik.errors.repeatNewPassword
+        }
       />
 
       <Button
