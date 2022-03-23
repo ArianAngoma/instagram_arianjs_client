e3036938d5a564610c98b214a24fe2289e388184
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-03-23 14:48:13 -0500

    Update email address added

--- src/components/User/EmailForm/EmailForm.tsx
+++ src/components/User/EmailForm/EmailForm.tsx
@@ -1,7 +1,11 @@
+import {ApolloError, useMutation} from '@apollo/client';
 import {Button, Form} from 'semantic-ui-react';
 import {FormikValues, useFormik} from 'formik';
 import * as Yup from 'yup';
 import './EmailForm.scss';
+
+import {UPDATE_USER} from '../../../gql/user';
+import {toast} from 'react-toastify';
 
 interface IProps {
   setShowModal: (showModal: boolean) => void;
@@ -9,6 +13,8 @@ interface IProps {
 }
 
 export const EmailForm = ({setShowModal, currentEmail}: IProps) => {
+  const [updateUser] = useMutation(UPDATE_USER);
+
   const formik = useFormik({
     initialValues: {
       email: currentEmail || '',
@@ -19,7 +25,21 @@ export const EmailForm = ({setShowModal, currentEmail}: IProps) => {
           .required('El email es obligatorio'),
     }),
     onSubmit: (formData: FormikValues) => {
-      console.log(formData);
+      updateUser({
+        variables: {
+          input: formData,
+        },
+      }).then(({data}) => {
+        if (!data.updateUser) {
+          toast.error('Error al actualizar el email');
+        } else {
+          toast.success('Email actualizado correctamente');
+          setShowModal(false);
+        }
+      }).catch((error: ApolloError) => {
+        console.log(error.message);
+        toast.error('Error al actualizar el email');
+      });
     },
   });
 
