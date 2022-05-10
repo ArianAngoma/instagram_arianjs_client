c72ba3d295ae031031cef5d30c34be6a77363bc8
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-05-10 12:03:00 -0500

    Open follower modal

--- src/components/User/Profile/Followers/Followers.tsx
+++ src/components/User/Profile/Followers/Followers.tsx
@@ -1,15 +1,20 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 import {useQuery} from '@apollo/client';
 
 import './Followers.scss';
 import {GET_FOLLOWERS} from '../../../../gql/follow';
+import {ModalBasic} from '../../../Modal/ModalBasic/ModalBasic';
 
 interface IProps {
   username: string;
 }
 
 export const Followers = ({username}: IProps) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [titleModal, setTitleModal] = useState<string>('');
+  const [childrenModal, setChildrenModal] = useState<any>(null);
+
   const {
     data: dataFollowers,
     loading: loadingFollowers,
@@ -24,19 +29,41 @@ export const Followers = ({username}: IProps) => {
     return () => stopPollingFollowers();
   }, [startPollingFollowers, stopPollingFollowers]);
 
+  const openFollowers = () => {
+    setTitleModal('Seguidores');
+    setChildrenModal(
+        <div>
+          <h3>Lista de seguidores</h3>
+        </div>,
+    );
+    setShowModal(true);
+  };
+
   if (loadingFollowers) return null;
 
   return (
-    <div className="followers">
-      <p>
-        <span>50</span> publicaciones
-      </p>
-      <p className="link">
-        <span>{dataFollowers.getFollowers.length}</span> seguidores
-      </p>
-      <p className="link">
-        <span>20</span> seguidos
-      </p>
-    </div>
+    <>
+      <div className="followers">
+        <p>
+          <span>50</span> publicaciones
+        </p>
+        <p
+          className="link"
+          onClick={openFollowers}
+        >
+          <span>{dataFollowers.getFollowers.length}</span> seguidores
+        </p>
+        <p className="link">
+          <span>20</span> seguidos
+        </p>
+      </div>
+      <ModalBasic
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title={titleModal}
+      >
+        {childrenModal}
+      </ModalBasic>
+    </>
   );
 };
